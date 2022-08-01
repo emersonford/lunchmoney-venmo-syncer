a5c4eaed01a4237f28ca192e0056c06d797e8ecf
Emerson Ford <emersontford@gmail.com>
2022-07-31 21:41:07 -0700

    add conversion logic from Venmo transaction to Lunch Money transaction

--- src/types/lunchmoney.rs
+++ src/types/lunchmoney.rs
@@ -1,6 +1,7 @@
 use std::fmt;
 use std::num::ParseFloatError;
 use std::str::FromStr;
+use std::time::UNIX_EPOCH;
 
 use chrono::{DateTime, Utc};
 use serde::{Deserialize, Serialize};
@@ -8,7 +9,7 @@ use serde::{Deserialize, Serialize};
 /// Tag object as described in https://lunchmoney.dev/#tags-object.
 #[derive(Debug, Serialize, Deserialize)]
 pub struct Tag {
-    pub id: i64,
+    pub id: u64,
     pub name: String,
     pub description: String,
 }
@@ -25,7 +26,7 @@ pub enum TransactionStatus {
 /// An f64 that serializes to a float up to 4 decimal places, as specified in the `Transaction`
 /// amount field description in https://lunchmoney.dev/#transaction-object.
 #[derive(Debug, Serialize, Deserialize)]
-pub struct Amount(f64);
+pub struct Amount(pub f64);
 
 impl FromStr for Amount {
     type Err = ParseFloatError;
@@ -50,19 +51,41 @@ impl From<f64> for Amount {
 /// Transaction object as defined in https://lunchmoney.dev/#transaction-object
 #[derive(Debug, Serialize, Deserialize)]
 pub struct Transaction {
-    pub id: i64,
+    pub id: Option<u64>,
     pub date: DateTime<Utc>,
     pub payee: Option<String>,
     pub amount: Amount,
     pub currency: Option<String>,
     pub notes: Option<String>,
-    pub category_id: Option<i64>,
-    pub asset_id: Option<i64>,
+    pub category_id: Option<u64>,
+    pub asset_id: Option<u64>,
     pub status: TransactionStatus,
-    pub parent_id: Option<i64>,
+    pub parent_id: Option<u64>,
     pub is_group: Option<bool>,
-    pub group_id: Option<i64>,
+    pub group_id: Option<u64>,
     pub tags: Option<Vec<Tag>>,
     pub external_id: Option<String>,
     pub original_name: Option<String>,
+}
+
+impl Default for Transaction {
+    fn default() -> Self {
+        Self {
+            id: None,
+            date: UNIX_EPOCH.into(),
+            payee: None,
+            amount: Amount(0.0),
+            currency: None,
+            notes: None,
+            category_id: None,
+            asset_id: None,
+            status: TransactionStatus::Uncleared,
+            parent_id: None,
+            is_group: None,
+            group_id: None,
+            tags: None,
+            external_id: None,
+            original_name: None,
+        }
+    }
 }
--- src/types/venmo.rs
+++ src/types/venmo.rs
@@ -3,8 +3,11 @@ use std::str::FromStr;
 use chrono::{DateTime, Utc};
 use lazy_static::lazy_static;
 use regex::Regex;
+use rusty_money::iso::Currency;
 use serde::{Deserialize, Serialize};
 use thiserror::Error;
+
+use super::lunchmoney;
 
 #[derive(Error, Debug)]
 pub enum Error {
@@ -14,9 +17,13 @@ pub enum Error {
     ParseStatusError(String),
     #[error("failed to parse Venmo amount: {0}")]
     ParseAmountError(String),
+    #[error("expected currency marker {0} for {1}, got {2} from Venmo")]
+    WrongCurrencyError(String, String, String),
+    #[error("expected field {0} to be defined due to {1} on record {2:?}")]
+    InvalidRecord(String, String, Transaction),
 }
 
-#[derive(Debug, Deserialize)]
+#[derive(Debug, Clone, Deserialize, PartialEq, Eq)]
 pub enum TransactionType {
     Charge,
     Payment,
@@ -40,7 +47,7 @@ impl FromStr for TransactionType {
     }
 }
 
-#[derive(Debug, Deserialize)]
+#[derive(Debug, Clone, Deserialize)]
 pub enum TransactionStatus {
     Complete,
     Issued,
@@ -64,7 +71,7 @@ lazy_static! {
     static ref VENMO_AMOUNT_RE: Regex = Regex::new(r"^(-?)([^0-9])([0-9.]+)$").unwrap();
 }
 
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct Amount {
     pub currency: String,
     pub val: f64,
@@ -92,11 +99,11 @@ impl FromStr for Amount {
 }
 
 /// Venmo transaction structure as found in their statement CSVs.
-#[derive(Debug, Deserialize)]
+#[derive(Debug, Deserialize, Clone)]
 #[serde(rename_all = "PascalCase")]
 pub struct Transaction {
     #[serde(rename = "ID")]
-    pub id: i64,
+    pub id: u64,
     pub datetime: DateTime<Utc>,
     #[serde(rename = "Type")]
     pub type_: TransactionType,
@@ -117,4 +124,140 @@ pub struct BalanceRecord {
     pub beginning_balance: Option<Amount>,
     #[serde(rename = "Ending Balance")]
     pub ending_balance: Option<Amount>,
+}
+
+impl Transaction {
+    fn to_lunchmoney_transactions(
+        &self,
+        expected_currency: Currency,
+        asset_id: u64,
+    ) -> Result<Vec<lunchmoney::Transaction>, Error> {
+        if self.total_amount.currency != expected_currency.symbol {
+            return Err(Error::WrongCurrencyError(
+                expected_currency.symbol.to_string(),
+                expected_currency.iso_alpha_code.to_string(),
+                self.total_amount.currency.clone(),
+            ));
+        }
+
+        let payee = match self.type_ {
+            TransactionType::StandardTransfer => self
+                .destination
+                .as_ref()
+                .map(|val| format!("TRANSFER TO {}", val))
+                .ok_or_else(|| {
+                    Error::InvalidRecord(
+                        "destination".to_string(),
+                        "'Transaction Type' is set to 'Standard Transfer'".to_string(),
+                        self.clone(),
+                    )
+                })?,
+            TransactionType::Charge => {
+                if self.total_amount.val.is_sign_positive() {
+                    self.to.as_ref().cloned().ok_or_else(|| {
+                        Error::InvalidRecord(
+                            "to".to_string(),
+                            "'Transaction Type' is set to 'Charge' and 'Amount' is positive"
+                                .to_string(),
+                            self.clone(),
+                        )
+                    })?
+                } else {
+                    self.from.as_ref().cloned().ok_or_else(|| {
+                        Error::InvalidRecord(
+                            "from".to_string(),
+                            "'Transaction Type' is set to 'Charge' and 'Amount' is negative"
+                                .to_string(),
+                            self.clone(),
+                        )
+                    })?
+                }
+            }
+            TransactionType::Payment | TransactionType::MerchantTransaction => {
+                if self.total_amount.val.is_sign_positive() {
+                    self.from.as_ref().cloned().ok_or_else(|| {
+                        Error::InvalidRecord(
+                            "from".to_string(),
+                            "'Transaction Type' is set to 'Payment' or 'Merchant Transaction' and 'Amount' is positive"
+                                .to_string(),
+                            self.clone(),
+                        )
+                    })?
+                } else {
+                    self.to.as_ref().cloned().ok_or_else(|| {
+                        Error::InvalidRecord(
+                            "to".to_string(),
+                            "'Transaction Type' is set to 'Payment' or 'Merchant Transaction' and 'Amount' is negative"
+                                .to_string(),
+                            self.clone(),
+                        )
+                    })?
+                }
+            }
+        };
+
+        let transactions = {
+            let mut txn = vec![lunchmoney::Transaction {
+                date: self.datetime,
+                payee: Some(payee),
+                amount: lunchmoney::Amount(self.total_amount.val),
+                currency: Some(expected_currency.iso_alpha_code.to_string()),
+                notes: self.note.as_ref().cloned(),
+                asset_id: Some(asset_id),
+                external_id: Some(self.id.to_string()),
+                status: lunchmoney::TransactionStatus::Uncleared,
+                ..Default::default()
+            }];
+
+            if let Some(ref funding_source) = self.funding_source {
+                if !funding_source.is_empty() && funding_source != "Venmo balance" {
+                    // Create a "shadow" transaction to indicate we transfered money from one
+                    // bank to our Venmo balance.
+                    txn.push(lunchmoney::Transaction {
+                        date: self.datetime,
+                        payee: Some(format!("TRANSFER FROM {}", funding_source)),
+                        amount: lunchmoney::Amount(-self.total_amount.val),
+                        currency: Some(expected_currency.iso_alpha_code.to_string()),
+                        notes: self
+                            .note
+                            .as_ref()
+                            .map(|val| format!("To fund Venmo transaction with note: '{}'", val)),
+                        asset_id: Some(asset_id),
+                        external_id: Some(format!("{}T", self.id)),
+                        status: lunchmoney::TransactionStatus::Uncleared,
+                        ..Default::default()
+                    });
+                }
+            }
+
+            if let Some(ref destination) = self.destination {
+                // It should never be possible to direct deposit a Venmo transaction to your bank
+                // account since Venmo always deposits it in your "Venmo balance" first... but just
+                // to cover our bases.
+                if !destination.is_empty()
+                    && destination != "Venmo balance"
+                    && self.type_ != TransactionType::StandardTransfer
+                {
+                    txn.push(lunchmoney::Transaction {
+                        date: self.datetime,
+                        payee: Some(format!("TRANSFER TO {}", destination)),
+                        amount: lunchmoney::Amount(-self.total_amount.val),
+                        currency: Some(expected_currency.iso_alpha_code.to_string()),
+                        notes: self
+                            .note
+                            .as_ref()
+                            .map(|val| format!("From Venmo transaction with note: '{}'", val)),
+                        asset_id: Some(asset_id),
+                        external_id: Some(format!("{}TDEPOSIT", self.id)),
+                        status: lunchmoney::TransactionStatus::Uncleared,
+                        ..Default::default()
+                    });
+                }
+            }
+
+            txn
+        };
+
+        Ok(transactions)
+    }
 }
